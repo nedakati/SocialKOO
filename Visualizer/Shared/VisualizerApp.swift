a39fa3c26a2264c6416c407d1b0263ec1fc6e6d2
Katalin Neda <katalin.neda@halcyonmobile.com>
2021-02-19 19:10:00 +0200

    Add project

---
+++ Visualizer/Shared/VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualizerApp.swift
+//  Shared
+//
+//  Created by Katalin Neda on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

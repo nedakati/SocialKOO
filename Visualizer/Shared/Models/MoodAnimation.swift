895388dfba123677ecd90b49125906318a28ace6
Fediuc <octavian.sergiu@gmail.com>
2021-02-21 19:53:48 +0200

    Meditation screen colors

--- Visualizer/Shared/Models/MoodAnimation.swift
+++ Visualizer/Shared/Models/MoodAnimation.swift
@@ -11,19 +11,19 @@ extension Intend {
 
     var mainColor: Color {
         switch self {
-        case .chillOut: return Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))
+        case .chillOut: return Color(#colorLiteral(red: 1, green: 0.3921568627, blue: 0.2470588235, alpha: 1))
         case .moodBoost: return Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))
-        case .stopWorrying: return Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))
+        case .stopWorrying: return Color(#colorLiteral(red: 1, green: 1, blue: 0.9490196078, alpha: 1))
         case .mindDistraction: return Color.random
         }
     }
     
     var gradients: [Color] {
         switch self {
-        case .chillOut: return [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))]
+        case .chillOut: return [Color(#colorLiteral(red: 0.6980392157, green: 0.7960784314, blue: 0.6078431373, alpha: 1)), Color(#colorLiteral(red: 0.6980392157, green: 0.7960784314, blue: 0.6078431373, alpha: 1)), Color(#colorLiteral(red: 0.3568627451, green: 0.5882352941, blue: 0.5960784314, alpha: 1))]
         case .moodBoost: return [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))]
-        case .stopWorrying: return [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))]
-        case .mindDistraction: return [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))]
+        case .stopWorrying: return [Color(#colorLiteral(red: 1, green: 0.8274509804, blue: 0.768627451, alpha: 1)), Color(#colorLiteral(red: 0.9215686275, green: 0.7529411765, blue: 0.4705882353, alpha: 1)), Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))]
+        case .mindDistraction: return [Color(#colorLiteral(red: 0, green: 1, blue: 0.9803921569, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.9411764741, green: 0.1892502156, blue: 0.3529411852, alpha: 1))]
         }
     }
     
